aecff54ff2d4cdd1350646da73781527c005de42
Adam Lane <adamsadlerlane@gmail.com>
2022-12-22 14:33:55 +0000

    sonarcloud bugfix 2.0

--- src/test/java/blueprints/actor_project/CoolProjectApplicationTests.java
+++ src/test/java/blueprints/actor_project/CoolProjectApplicationTests.java
@@ -3,11 +3,9 @@ package blueprints.actor_project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CoolProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
