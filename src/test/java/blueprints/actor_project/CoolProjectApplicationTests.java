45c5a69820d72bad2db4129082c8878d4b19e64c
Adam Lane <adamsadlerlane@gmail.com>
2022-12-22 15:45:05 +0000

    Unit tests for Actor class

--- src/test/java/blueprints/actor_project/CoolProjectApplicationTests.java
+++ src/test/java/blueprints/actor_project/CoolProjectApplicationTests.java
@@ -1,11 +1,21 @@
 package blueprints.actor_project;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 class CoolProjectApplicationTests {
 
 
+    @Test
+    public void testGettersAndSetters(){
+        Actor actor1 = new Actor();
+        actor1.setActorid(5);
+        Assertions.assertEquals(5,actor1.getActorid(), "Actor id error");
+        actor1.setFirstname("Firstnametest");
+        Assertions.assertEquals("Firstnametest",actor1.getFirstname(), "First name error");
+        actor1.setLastname("Lastnametest");
+        Assertions.assertEquals("Lastnametest",actor1.getFirstname(), "Last name error");
+    }
 
 }
