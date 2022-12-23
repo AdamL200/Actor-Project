aa67cbed8b19691f368adf8eb04f0e4c4a824b4f
Adam Lane <adamsadlerlane@gmail.com>
2022-12-23 09:29:23 +0000

    Unit tests category class

--- src/test/java/blueprints/actor_project/CoolProjectApplicationTests.java
+++ src/test/java/blueprints/actor_project/CoolProjectApplicationTests.java
@@ -8,7 +8,7 @@ class CoolProjectApplicationTests {
 
 
     @Test
-    public void testGettersAndSetters(){
+    public void testActorGettersAndSetters(){
         Actor actor1 = new Actor();
         actor1.setActorid(5);
         Assertions.assertEquals(5,actor1.getActorid(), "Actor id error");
@@ -17,5 +17,26 @@ class CoolProjectApplicationTests {
         actor1.setLastname("Lastnametest");
         Assertions.assertEquals("Lastnametest",actor1.getLastname(), "Last name error");
     }
+
+    @Test
+    public void testError(){
+        ActorNotFoundException exception1 = new ActorNotFoundException(3);
+
+        String expectedMessage = "Could not find actor";
+        String actualMessage = exception1.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testCategoryGettersAndSetters(){
+        Category cat1 = new Category();
+        cat1.setCatid(2);
+        Assertions.assertEquals(2,cat1.getCatid(), "Category id error");
+        cat1.setCatName("Action");
+        Assertions.assertEquals("Action",cat1.getCatName(), "Category name error");
+
+    }
+
 
 }
